33934c53bdeb5b9c2298d986a2d1d9541b8bd4f1
Andrew Watson <andrew.watson@ufl.edu>
2020-04-30 02:58:52 -0400

    Added license notice

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+/*
+    GBEmu - A simple DMG emulator
+    Copyright (C) 2020 Andrew Watson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 #pragma ide diagnostic ignored "hicpp-signed-bitwise"
@@ -42,6 +61,7 @@ const unsigned int updateFrequency = 25000; //update every 5k cycles
 
 int main(int argc, char* args[])
 {
+	cout << "GBEmu  Copyright (C) 2020 Andrew Watson\nThis program comes with ABSOLUTELY NO WARRANTY; for details, see the included LICENSE file or visit https://www.gnu.org/licenses/ \n";
     cout<<"Initializing window..."<<endl;
     initWindow();
     cout << "Initializing audio..." << endl;
